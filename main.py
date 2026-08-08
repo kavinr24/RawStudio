cae92598aa79b684a3642c1d8f7a7adb8440c588
kavin <kavinr2400@gmail.com>
2026-08-08 16:22:45 -0700

    add dark background and vertical layout

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
 
 app = QApplication(sys.argv)
 
 window = QWidget()
 window.setWindowTitle("RawStudio")
-window.resize(1000,650)
+window.resize(1000, 650)
+window.setStyleSheet("background-color: #1e1e1e;")
 
-label = QLabel("RawStudio",parent=window)
-label.move(450,300)
+layout = QVBoxLayout()
+
+label = QLabel("RawStudio")
+label.setStyleSheet("color: #ffffff; fontize: 16px;")
+
+layout.addWidget(label)
+window.setLayout(layout)
 
 window.show()
+
 sys.exit(app.exec())
+
